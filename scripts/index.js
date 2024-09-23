69f99c95bd426428bae565b17aa7f83e010e9aaa
Daria Ogareva <ogareva.darya@bk.ru>
2024-09-23 06:01:54 +0300

    v1.1

--- scripts/index.js
+++ scripts/index.js
@@ -7,15 +7,15 @@ const listCard = document.querySelector('.places__list');
 const buttonAddCard = document.querySelector('.profile__add-button');
 
 // @todo: Функция создания карточки
-function cardAdd(name, link) {
+function cardAdd(name, link, cardDelete) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   const cardTitle = cardElement.querySelector('.card__title');
   const cardImage = cardElement.querySelector('.card__image');
   cardTitle.textContent = name;
-  cardImage.setAttribute('src',link);
-  cardImage.setAttribute('alt',name);
+  cardImage.setAttribute('src', link);
+  cardImage.setAttribute('alt', name);
   cardElement.querySelector('.card__delete-button').addEventListener('click' , () => {
-    cardDelete(cardElement);
+    cardDelete(cardElement)
   });
   return cardElement ;
 };
@@ -26,9 +26,9 @@ function cardDelete(cardElement) {
 }
 
 // @todo: Вывести карточки на страницу
-function cardRender(container,cardData) {
+function cardRender(container, cardData) {
   container.append(cardData);
 }
 initialCards.forEach((obj) => {
-  cardRender(listCard, cardAdd(obj.name, obj.link));
+  cardRender(listCard, cardAdd(obj.name, obj.link, cardDelete));
 })
